f3b2856739644bc74faec8a673589353cc80d6e6
Petrik Lajos Szg <info@petrik.hu>
2019-09-27 16:45:11 +0200

    Initial commit

---
+++ AukcioProjekt/Festmeny.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AukcioProjekt
+{
+    class Festmeny
+    {
+        private String cim;
+        private String festo;
+        private String stilus;
+        private int licitekSzama;
+        private int legmagasabbLicit;
+        private DateTime legutolsoLicitIdeje;
+        private bool elkelt;
+
+        public Festmeny(string cim, string festo, string stilus)
+        {
+            this.cim = cim;
+            this.festo = festo;
+            this.stilus = stilus;
+            this.licitekSzama = 0;
+            this.legmagasabbLicit = 0;
+            this.elkelt = false;
+        }
+
+        public string Festo { get => festo; }
+        public string Stilus { get => stilus; } 
+        public int LicitekSzama { get => licitekSzama; }
+        public int LegmagasabbLicit { get => legmagasabbLicit; }
+        public DateTime LegutolsoLicitIdeje { get => legutolsoLicitIdeje; }
+        public bool Elkelt { get => elkelt; set => elkelt = value; }
+
+        public void Licit()
+        {
+            if (elkelt)
+            {
+                Console.WriteLine("Hiba. A termék már elkelt.");
+            }
+            else if (licitekSzama == 0)
+            {
+                this.legmagasabbLicit = 100;
+                this.licitekSzama++;
+                this.legutolsoLicitIdeje = DateTime.Now;
+            }
+            else
+            {
+                licitekSzama++;
+                legmagasabbLicit += legmagasabbLicit / 10;
+                this.legutolsoLicitIdeje = DateTime.Now;
+            }
+        }
+
+        public void Licit(int mertek)
+        {
+            if (mertek > 9 && mertek < 101)
+            {
+                if (licitekSzama == 0)
+                {
+                    legmagasabbLicit = 100;
+                }
+                licitekSzama++;
+                legmagasabbLicit += (legmagasabbLicit / 100) * mertek;
+                this.legutolsoLicitIdeje = DateTime.Now;
+            }
+            else
+            {
+                Console.WriteLine("Hiba, rossz szám.");
+            }
+        }
+
+        
+
+        
+        public override string ToString()
+        {
+            String s = "";
+            String elkeltE = "";
+            if (elkelt)
+            {
+                elkeltE = "Elkelt";
+            }
+            else
+            {
+                elkeltE = "Nem kelt el";
+            }
+            s += "Festo: " + Festo + " (" + stilus + ")\n"
+                + elkeltE + "\n"
+                + "legmagasabbLicit " + legmagasabbLicit + "$ - "
+                + "LegutolsoLicitIdeje " + legutolsoLicitIdeje
+                + " (összesen: " + licitekSzama + " db)";
+
+            return s;
+        }
+    }
+}
---
+++ AukcioProjekt/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AukcioProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Festmeny f = new Festmeny("Asd", "a", "e");
+            //f.Licit();
+            //f.Licit(10);
+            //Console.WriteLine(f.ToString());
+            //f.Licit(50);
+            //Console.WriteLine(f.ToString());
+            //f.Elkelt = true;
+            //Console.WriteLine(f.ToString());
+
+            List<Festmeny> lista = new List<Festmeny>();
+            Random r = new Random();
+            int mertek = 0;
+
+            lista.Add(new Festmeny("Asd", "as", "ad"));
+            lista.Add(new Festmeny("Qwe", "qw", "qe"));
+
+            Console.WriteLine("Kérem adja meg hány új festményt akar: ");
+            int db = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < db; i++)
+            {
+                Console.WriteLine("Cim: ");
+                String cim = Console.ReadLine();
+                Console.WriteLine("Festo: ");
+                String festo = Console.ReadLine();
+                Console.WriteLine("stilus: ");
+                String stilus = Console.ReadLine();
+                lista.Add(new Festmeny(cim, festo, stilus));
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < lista.Count; j++)
+                {
+                    lista[j].Licit(r.Next(10, 100));
+                }
+            }
+
+            foreach (var a in lista)
+            {
+                Console.WriteLine(a.ToString());
+            }
+
+            Console.WriteLine("Adja meg hanyadkat akarja modositani: ");
+            int valasz = int.Parse(Console.ReadLine());
+
+            while (valasz != 0)
+            {
+                Console.WriteLine("Milyen mértékkel szeretne licitálni: ");
+                mertek = int.Parse(Console.ReadLine());
+
+                lista[valasz-1].Licit(mertek);
+                lista[valasz-1].Elkelt = true;
+
+
+                Console.WriteLine("Adja meg hanyadkat akarja modositani: ");
+                valasz = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var a in lista)
+            {
+                Console.WriteLine(a.ToString());
+            }
+
+            String s = "";
+            int max = lista[0].LegmagasabbLicit;
+            Festmeny f = lista[0];
+
+            foreach (var item in lista)
+            {
+                if (item.LegmagasabbLicit > max)
+                {
+                    max = item.LegmagasabbLicit;
+                    f = item;
+                }
+                
+
+            }
+            
+            Console.WriteLine("3.a.: \nFesto: " + f.Festo + " (" + f.Stilus + ")\n"
+                + f.Elkelt + "\n"
+                + "legmagasabbLicit " + f.LegmagasabbLicit + "$ - "
+                + "LegutolsoLicitIdeje " + f.LegutolsoLicitIdeje
+                + " (összesen: " + f.LicitekSzama + " db)");
+
+
+
+            int asd = 0;
+            bool tizLicit = false;
+            while (asd < lista.Count && !tizLicit)
+            {
+                if (lista[asd].LicitekSzama > 10)
+                {
+                    tizLicit = true;
+                }
+
+
+
+                asd++;
+            }
+            Console.WriteLine("3. b.: " + tizLicit);
+
+
+            int szam = 0;
+            foreach (var item in lista)
+            {
+                if (item.Elkelt == false)
+                {
+                    szam++;
+                }
+            }
+            Console.WriteLine("3. c.: " + szam);
+
+
+            lista = lista.OrderByDescending(i => i.LegmagasabbLicit).ToList();
+
+            foreach (var a in lista)
+            {
+                Console.WriteLine(a.ToString());
+            }
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
